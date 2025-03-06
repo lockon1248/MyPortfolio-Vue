e29a07c96a7b358a82df357f0f9ad66436ef7106
lockon <lockon5445@gmail.com>
2025-03-07 01:44:13 +0800

    feat: 新增 vite-plugin-image-optimizer 套件以壓縮打包體積
    1. 新安裝 vite-plugin-image-optimizer 並於 vite.config.ts 使用該插件來優化圖片打包體積
    2. 註解 router\index.js 中動態引入 App.vue 方法，避免與 main.js 靜態引入 App.vue 發生衝突

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,13 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 export const router = createRouter({
 	history: createWebHashHistory(),
 	routes: [
-		{
-			path: '/',
-			name: 'MyPortfolio',
-			component: () => import('@/App.vue'),
-			meta: {
-				title: 'My Portfolio'
-			}
-		}
+		// {
+		// 	path: '/',
+		// 	name: 'MyPortfolio',
+		// 	component: () => import('@/App.vue'),
+		// 	meta: {
+		// 		title: 'My Portfolio'
+		// 	}
+		// }
 	]
 });
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,14 @@ export default defineConfig({
 			symbolId: 'icon-[dir]-[name]', // 指定symbolId格式 預設：'icon-[dir]-[name]
 			inject: 'body-last' | 'body-first', // 自定义插入位置
 			customDomId: '__svg__icons__dom__' // 自訂 Dom ID
+		}),
+		ViteImageOptimizer({
+			png: {
+				compressionLevel: 9, // 范围 0-9，9 是最高压缩，完全无损
+			},
+			jpg: {
+				compressionLevel: 9, // 范围 0-9，9 是最高压缩，完全无损
+			}
 		})
 	],
 	resolve: {
